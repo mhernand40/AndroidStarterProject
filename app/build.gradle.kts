b28ef3b520f7451b7d0c6d3c2308dd4cc1bee8f2
Marcelo Hernandez <mhernand40@gmail.com>
2023-11-17 01:35:27 -0500

    Reformat gradle scripts with ktfmt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,55 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.hiltAndroidGradlePlugin)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.ksp)
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.hiltAndroidGradlePlugin)
+  alias(libs.plugins.jetbrainsKotlinAndroid)
+  alias(libs.plugins.ksp)
 }
 
 android {
-    buildFeatures {
-        compose = true
-    }
+  buildFeatures { compose = true }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.androidxComposeCompiler.get().version
-    }
+  composeOptions { kotlinCompilerExtensionVersion = libs.androidxComposeCompiler.get().version }
 
-    namespace = "com.starter"
-    compileSdk = 34
+  namespace = "com.starter"
+  compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.starter"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.starter"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs += "-Xexplicit-api=strict"
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+    freeCompilerArgs += "-Xexplicit-api=strict"
+  }
 }
 
 dependencies {
-    debugImplementation(libs.androidxComposeUiTooling)
+  debugImplementation(libs.androidxComposeUiTooling)
 
-    implementation(libs.androidxActivityCompose)
-    implementation(libs.androidxComposeMaterial3)
-    implementation(libs.androidxComposeUiToolingPreview)
-    implementation(libs.androidxCoreKtx)
-    implementation(libs.androidxLifecycleViewModelCompose)
-    implementation(libs.material)
-    implementation(libs.hiltAndroid)
-    implementation(platform(libs.androidxComposeBom))
+  implementation(libs.androidxActivityCompose)
+  implementation(libs.androidxComposeMaterial3)
+  implementation(libs.androidxComposeUiToolingPreview)
+  implementation(libs.androidxCoreKtx)
+  implementation(libs.androidxLifecycleViewModelCompose)
+  implementation(libs.material)
+  implementation(libs.hiltAndroid)
+  implementation(platform(libs.androidxComposeBom))
 
-    ksp(libs.hiltCompiler)
+  ksp(libs.hiltCompiler)
 }
